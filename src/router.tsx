91db3e76c819cf6974d27b7c64a6193e1a8a2ecb
oc-garcia <octaviogarcia1988@gmail.com>
2024-08-20 00:17:12 -0300

    first commit

---
+++ src/router.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
