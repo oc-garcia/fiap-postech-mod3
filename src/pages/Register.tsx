3cedcc495098c4cdb80190ec9179b33493979978
oc-garcia <octaviogarcia1988@gmail.com>
2024-08-22 01:23:54 -0300

    chore: Update login and register pages
    This commit updates the login and register pages by adding Chakra UI components and form validation using Formik. The pages now have a centered layout with card components containing form fields for email, password, and confirm password. The submit buttons have been styled with a pink background color.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,103 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Center,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
+import { Field, Formik } from "formik";
+
 export default function Register() {
   return (
-    <div>Register</div>
-  )
+    <Center flex={"1"}>
+      <Card>
+        <CardHeader>
+          <Heading size="md">Register to Continue</Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Stack spacing="4">
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+                rememberMe: false,
+              }}
+              onSubmit={(values) => {
+                alert(JSON.stringify(values, null, 2));
+              }}>
+              {({ handleSubmit, errors, touched }) => (
+                <form onSubmit={handleSubmit}>
+                  <VStack spacing={4} align="flex-start">
+                    <FormControl>
+                      <FormLabel htmlFor="email">Email Address</FormLabel>
+                      <Field as={Input} id="email" name="email" type="email" variant="filled" />
+                    </FormControl>
+                    <FormControl isInvalid={!!errors.password && touched.password}>
+                      <FormLabel htmlFor="password">Password</FormLabel>
+                      <Field
+                        as={Input}
+                        id="password"
+                        name="password"
+                        type="password"
+                        variant="filled"
+                        validate={(value) => {
+                          let error;
+                          if (value.length < 6) {
+                            error = "Password must contain at least 6 characters";
+                          }
+                          return error;
+                        }}
+                      />
+                      <FormErrorMessage>{errors.password}</FormErrorMessage>
+                    </FormControl>
+                    <FormControl isInvalid={!!errors.password && touched.password}>
+                      <FormLabel htmlFor="password">Confirm Password</FormLabel>
+                      <Field
+                        as={Input}
+                        id="password"
+                        name="password"
+                        type="password"
+                        variant="filled"
+                        validate={(value) => {
+                          let error;
+                          if (value.length < 6) {
+                            error = "Password must contain at least 6 characters";
+                          }
+                          return error;
+                        }}
+                      />
+                      <FormErrorMessage>{errors.password}</FormErrorMessage>
+                    </FormControl>
+                    <Button
+                      type="submit"
+                      fontSize={"sm"}
+                      fontWeight={600}
+                      color={"white"}
+                      bg={"pink.400"}
+                      width="full"
+                      _hover={{
+                        bg: "pink.300",
+                      }}>
+                      Login
+                    </Button>
+                    <Button as={"a"} fontSize={"sm"} fontWeight={400} variant={"link"} href={"/login"}>
+                      Already registered?
+                    </Button>
+                  </VStack>
+                </form>
+              )}
+            </Formik>
+          </Stack>
+        </CardBody>
+      </Card>
+    </Center>
+  );
 }
