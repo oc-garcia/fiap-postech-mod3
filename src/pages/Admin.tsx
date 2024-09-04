b6cca771c6106cc87e4ca0b74c797a5cb70058c1
oc-garcia <octaviogarcia1988@gmail.com>
2024-09-03 21:04:17 -0300

    chore: Update Admin and Home pages with Chakra UI Tabs component
    This commit updates the Admin and Home pages to use the Chakra UI Tabs component for displaying tabbed content. The Tabs component provides a user-friendly interface for switching between different sections of the page. The existing content has been wrapped in TabList, TabPanel, and TabPanels components to create the tabbed layout. This improves the overall user experience and makes the pages more interactive.

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,5 +1,105 @@
+import { Flex, Skeleton, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
+import { IPost } from "../interfaces/IPost";
+import axios, { AxiosResponse } from "axios";
+import { PostsService } from "../services/PostsService";
+
 export default function Admin() {
+  const context = useContext(AppContext);
+  const navigate = useNavigate();
+
+  if (!context) {
+    throw new Error("MyComponent must be used within an AppProvider");
+  }
+
+  const { setToken } = context;
+
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+
+    const getAllPosts = async () => {
+      try {
+        const response: AxiosResponse<IPost[]> = await PostsService.getAll();
+        setPosts(response.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          console.error("Failed to fetch posts", error.response?.data || error.message);
+        } else {
+          console.error("Failed to fetch posts", error);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (token) {
+      setToken(token);
+      getAllPosts();
+    } else {
+      setLoading(false);
+      navigate("/login");
+    }
+  }, [setToken, navigate]);
+
+  if (loading) {
+    return (
+      <Flex minHeight={"50vh"} flexDirection={"column"} gap={"1rem"}>
+        <Skeleton flex={"1"} />
+        <Skeleton flex={"1"} />
+        <Skeleton flex={"1"} />
+      </Flex>
+    );
+  }
   return (
-    <div>Admin</div>
-  )
+    <Flex flexDirection={"column"} gap={"1rem"}>
+      <Tabs isFitted variant="unstyled">
+        <TabList mb="1em">
+          <Tab
+            borderBottom="1px solid"
+            borderColor="pink.400"
+            fontWeight="bold"
+            _selected={{
+              color: "pink.400",
+              borderColor: "pink.400",
+              borderTop: "1px solid",
+              borderLeft: "1px solid",
+              borderRight: "1px solid",
+              borderBottom: "1px solid transparent",
+              background: "rgba(255, 192, 203,  0.05)",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            }}>
+            Post Manager
+          </Tab>
+          <Tab
+            borderBottom="1px solid"
+            borderColor="pink.400"
+            fontWeight="bold"
+            _selected={{
+              color: "pink.400",
+              borderTop: "1px solid",
+              borderLeft: "1px solid",
+              borderRight: "1px solid",
+              borderBottom: "1px solid transparent",
+              background: "rgba(255, 192, 203, 0.05)",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            }}>
+            User Manager
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Flex>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,12 +16,19 @@ import {
   Skeleton,
   Stack,
   StackDivider,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   Text,
+  useColorModeValue,
 } from "@chakra-ui/react";
 
 export default function Home() {
   const context = useContext(AppContext);
   const navigate = useNavigate();
+  const tabListColor = useColorModeValue("gray.600", "gray.200");
 
   if (!context) {
     throw new Error("MyComponent must be used within an AppProvider");
@@ -70,30 +77,74 @@ export default function Home() {
   }
 
   return (
-    <Flex flexDirection={"column"} gap={"1rem"}>
-      {posts.map((post) => (
-        <Card key={post.id}>
-          <CardHeader>
-            <Heading size="md">{post.title}</Heading>
-          </CardHeader>
-          <Divider />
-          <CardBody>
-            <Box>
-              <Text pt="2" fontSize="sm">
-                {post.content}
-              </Text>
-            </Box>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <Stack direction="row" divider={<StackDivider borderColor="gray.200" />} spacing={4}>
-              <Text>Author: {post.author}</Text>
-              {post.creation_date && <Text>Created: {new Date(post.creation_date).toLocaleString("pt-BR")}</Text>}
-              {post.update_date && <Text>Updated: {new Date(post.update_date).toLocaleString("pt-BR")}</Text>}
-            </Stack>
-          </CardFooter>
-        </Card>
-      ))}
+    <Flex flexDirection={"column"}>
+      <Tabs isFitted variant="unstyled">
+        <TabList mb="1em" color={tabListColor}>
+          <Tab
+            borderBottom="1px solid"
+            borderColor="pink.400"
+            fontWeight="bold"
+            _selected={{
+              color: "pink.400",
+              borderColor: "pink.400",
+              borderTop: "1px solid",
+              borderLeft: "1px solid",
+              borderRight: "1px solid",
+              borderBottom: "1px solid transparent",
+              background: "rgba(255, 192, 203,  0.05)",
+            }}>
+            All Posts
+          </Tab>
+          <Tab
+            borderBottom="1px solid"
+            borderColor="pink.400"
+            fontWeight="bold"
+            _selected={{
+              color: "pink.400",
+              borderTop: "1px solid",
+              borderLeft: "1px solid",
+              borderRight: "1px solid",
+              borderBottom: "1px solid transparent",
+              background: "rgba(255, 192, 203, 0.05)",
+            }}>
+            Search Posts
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel padding={0}>
+            <Flex flexDirection={"column"} gap={3}>
+              {posts.map((post) => (
+                <Card key={post.id}>
+                  <CardHeader>
+                    <Heading size="md">{post.title}</Heading>
+                  </CardHeader>
+                  <Divider />
+                  <CardBody>
+                    <Box>
+                      <Text pt="2" fontSize="sm">
+                        {post.content}
+                      </Text>
+                    </Box>
+                  </CardBody>
+                  <Divider />
+                  <CardFooter>
+                    <Stack direction="row" divider={<StackDivider borderColor="gray.200" />} spacing={4}>
+                      <Text>Author: {post.author}</Text>
+                      {post.creation_date && (
+                        <Text>Created: {new Date(post.creation_date).toLocaleString("pt-BR")}</Text>
+                      )}
+                      {post.update_date && <Text>Updated: {new Date(post.update_date).toLocaleString("pt-BR")}</Text>}
+                    </Stack>
+                  </CardFooter>
+                </Card>
+              ))}
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Flex>
   );
 }
